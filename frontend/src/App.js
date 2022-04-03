6f5c5cc27789ad4e054fda97c643e8fe5f3998f7
vaibhava03 <99650989+vaibhava03@users.noreply.github.com>
2022-04-03 14:36:46 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,32 @@ import axios from 'axios';
 import { useState } from 'react';
 import { useEffect } from 'react';
 function App() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const input = document.getElementById("Pwd");
+    const name=document.getElementById('Name').value;
+    const email=document.getElementById('email').value;
+    const phoneNumber=document.getElementById('phoneNumber').value;
+    const password=input.value;
+    const myObj={
+      name:name,
+      email:email,
+      phone:phoneNumber,
+      password:password
+    }
+
+    axios.post('http://localhost:4000/user/signup', myObj)
+    .then(res =>{
+      if(res.status===201)
+      {
+        alert('successfully signed up');
+      }
+      else if(res.status===208)
+      {
+        alert('user already exists please login');
+      }
+    }).catch(err => console.log(err));
+  }
 
 
   return (
@@ -13,9 +39,8 @@ function App() {
       </header>
       <div className="formDiv">
         <div>
-          
         </div>
-      <form id="Form">
+      <form id="Form" onSubmit={handleSubmit}>
         <label for="UserName" >User Name:</label> <br />  
           <input type="text" id="Name" placeholder="Enter a Username" /><br />
           <label for="email">Email:</label><br />
